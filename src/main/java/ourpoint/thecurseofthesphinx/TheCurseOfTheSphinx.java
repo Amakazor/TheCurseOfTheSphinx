20ebccfe65880c6eb26c4243202b0de4303d3bc5
amakazor <amakazor@gmail.com>
2020-08-11 00:58:28 +0200

    cleaning main class

--- src/main/java/ourpoint/thecurseofthesphinx/TheCurseOfTheSphinx.java
+++ src/main/java/ourpoint/thecurseofthesphinx/TheCurseOfTheSphinx.java
@@ -1,22 +1,13 @@
 package ourpoint.thecurseofthesphinx;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DeferredWorkQueue;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -24,9 +15,6 @@ import ourpoint.thecurseofthesphinx.entity.MummyEntity;
 import ourpoint.thecurseofthesphinx.init.TCOTSEntityTypes;
 import ourpoint.thecurseofthesphinx.init.TCOTSInit;
 import ourpoint.thecurseofthesphinx.init.TCOTSItems;
-import ourpoint.thecurseofthesphinx.items.TCOTSSpawnEggItem;
-
-import java.util.stream.Collectors;
 
 @Mod (TheCurseOfTheSphinx.MOD_ID)
 public class TheCurseOfTheSphinx
@@ -59,7 +47,7 @@ public class TheCurseOfTheSphinx
     }
 
     //Creative Tab
-    public static final ItemGroup TAB = new ItemGroup("TCOTSTab")
+    public static final ItemGroup TAB = new ItemGroup("tcotstab")
     {
         @Override
         public ItemStack createIcon()
